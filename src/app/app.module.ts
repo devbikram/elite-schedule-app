9e5d2113d85eaac2b4d0016675c502072c2912e4
Michael Sevestre <michael.sevestre@gmail.com>
2016-11-07 12:49:32 -0500

    End of navigation with ionic2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { MyTeamsPage, TournamentsPage, TeamsPage, TeamHomePage, TeamDetailPage,StandingsPage } from '../pages/pages';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    TournamentsPage,
+    TeamsPage,
+    TeamHomePage,
+    TeamDetailPage,
+    StandingsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +19,12 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    TournamentsPage,
+    TeamsPage,
+    TeamHomePage,
+    TeamDetailPage,
+    StandingsPage
   ],
   providers: []
 })
